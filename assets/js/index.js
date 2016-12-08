97297787584b1db26f7904936dfb9901623fb2c1
AldoTu <aldoturegano10@hotmail.com>
2016-12-08 15:34:30 -0600

    Se crea contactos y parte de productos.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
+
+  var count = 0;
+  //var imageID=0;
+
+  //var intros = ['/assets/img/intro.jpg', '/assets/img/intro1.jpg', '/assets/img/intro2.jpg'];
+
   $('h1').animate({left: 0}, 1700);
-  
+
   $(".link-container").mouseover(function(){
     $(this).css({
       transition : 'background-color 0.3s ease-in-out', "background-color": "white"
@@ -17,6 +23,28 @@ $(document).ready(function(){
     $(this).children().css({
       transition : 'color 0.3s ease-in-out', "color": "white"
     });
+  });
+
+  //$('.link-container').click(function(){
+    //$(this).css("background-color", "white");
+    //$(this).css("color", "rgba(0, 0, 0, 0.5)");
+  //});
+
+  $("#footer-arrow").click(function(){
+    if (count == 0) {
+      $("header").animate({top: -110}, 425);
+      $('.page-container').animate({bottom: 200}, 425);
+      //$("#footer-arrow").attr('transform', 'translate(0 0) rotate(180)');
+      //$("#footer-arrow").animate({svgTransform: 'translate(0 0) rotate(180)'});
+      count = 1;
+    }
+    else if (count == 1) {
+      $("header").animate({top: 0}, 425);
+      $('.page-container').animate({bottom: 0}, 425);
+      //$("#footer-arrow").attr('transform', 'translate(0 0) rotate(180)');
+      //$("#footer-arrow").animate({svgTransform: 'translate(0 0) rotate(180)'});
+      count = 0;
+    }
   });
 
 });
